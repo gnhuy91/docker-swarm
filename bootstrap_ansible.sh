fd179599b2d1c3f46f8a4c8a4b9a052244d35cf5
Huy Giang <gnhuy91@gmail.com>
2016-04-30 13:54:39 +0700

    install Ansible via apt
    b/c install using pip recently caused lots of errors (cffi and
    setuptools etc.)

--- bootstrap_ansible.sh
+++ bootstrap_ansible.sh
@@ -5,9 +5,10 @@ export DEBIAN_FRONTEND=noninteractive
 trap 'exit' ERR
 
 echo "Installing Ansible..."
-apt-get update -y -qq
-apt-get install -y -qq python-pip python-dev sshpass
-pip install -q ansible==2.0.1.0
+apt-get install -y --force-yes -qq software-properties-common
+apt-add-repository -y ppa:ansible/ansible
+apt-get update -qq
+apt-get install -y -qq ansible
 
 echo "Run Ansible playbook..."
 cd /vagrant/ansible && ansible-playbook swarm.yml -i hosts
