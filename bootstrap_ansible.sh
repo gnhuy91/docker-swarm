2196360f9671a220610893d585ee41b0ecac96d1
Huy Giang <gnhuy91@gmail.com>
2016-06-22 09:14:50 +0700

    reinstall ca-certificates to ensure adding paa ok

--- bootstrap_ansible.sh
+++ bootstrap_ansible.sh
@@ -5,6 +5,7 @@ export DEBIAN_FRONTEND=noninteractive
 trap 'exit' ERR
 
 echo "Installing Ansible..."
+apt-get install -y -qq --reinstall ca-certificates
 apt-get install -y --force-yes -qq software-properties-common
 apt-add-repository -y ppa:ansible/ansible
 apt-get update -qq
